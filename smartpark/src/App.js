e08a0deed1f3a71708d83b67ef7f012bf7cc5965
DineshkumarKumavat <dkumawat422@gmail.com>
2025-02-07 00:03:43 -0600

    CSS and JS file changed

--- smartpark/src/App.js
+++ smartpark/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import img1 from "./img1.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Smart Park blockchain
-      </h1>
+    <div className="app-container">
+      <h1>Smart Park Blockchain</h1>
+      <img src={img1} alt="Smart Park" />
     </div>
   );
 }
